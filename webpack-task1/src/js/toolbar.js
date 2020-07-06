02c55592a86203bc43a4bc3325a933b8e1394f16
11alexey11 <65157290+11alexey11@users.noreply.github.com>
2020-07-06 10:28:44 +0300

    Add files via upload

---
+++ webpack-task1/src/js/toolbar.js
@@ -0,0 +1,17 @@
+function chooseMode(mode, el) {
+    const activeText = document.getElementsByClassName("activeText");
+    for (let i = 0; i < activeText.length; i++) {
+        activeText[i].style.display = "none";
+    }
+
+    const barButton = document.getElementsByClassName("barButton");
+    for (let i = 0; i < barButton.length; i++) {
+        barButton[i].style.color = "";
+    }
+    document.getElementById(mode).style.display = "block";
+    el.style.color = '#3CB371';
+}
+
+document.getElementById("btnAllVoices").click();
+
+module.exports = {chooseMode};
