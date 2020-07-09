2f5afd6f796a26c532f9010c1ebcf2c8be5a6189
11alexey11 <65157290+11alexey11@users.noreply.github.com>
2020-07-09 23:31:24 +0300

    Update app.js

--- webpack-task1/src/app.js
+++ webpack-task1/src/app.js
@@ -1,7 +1,6 @@
 import './scss/base.scss';
 import '../dist/style.css';
-import {chooseMode} from './js/toolbar.js';
-
+import {chooseMode, voiceBtnMicrophone, getStream, getAllVoices} from './js/toolbar.js';
 const btnAllVoices = document.getElementById("btnAllVoices");
 const btnMicrophone = document.getElementById("btnMicrophone");
 const btnStream = document.getElementById("btnStream");
@@ -9,5 +8,9 @@ const btnStream = document.getElementById("btnStream");
 btnAllVoices.addEventListener("click", function(){chooseMode('AllVoices', this)});
 btnMicrophone.addEventListener("click", function(){chooseMode('Microphone', this)});
 btnStream.addEventListener("click", function(){chooseMode('Stream', this)});
+
+btnMicrophone.addEventListener("click", function(){voiceBtnMicrophone()});
+btnStream.addEventListener("click", function(){getStream()});
+btnAllVoices.addEventListener("click", function(){getAllVoices()});
 
 chooseMode('AllVoices', btnAllVoices);
